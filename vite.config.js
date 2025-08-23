bcb3257733baf0a64c78bc64b927f18f9070b0b1
Richard Spencer <contact@spencerdeals.bm>
2025-08-23 10:02:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['sdl-quote-frontend-production.up.railway.app'],
-    host: true,
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,
-  },
   server: {
-    host: true,
+    port: 5173,
+    open: false,
   },
 })
